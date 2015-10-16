78eca793b3c4ae5936ff1c4c70737e643774fbfc
Rick <rick.ramirez6174@gmail.com>
2015-10-16 13:03:13 -0700

    Finished Assignment 3

--- src/Assignments/active.java
+++ src/Assignments/active.java
@@ -15,8 +15,8 @@ public class active extends PApplet {
     int topY = 0;
     int bottomX = 500;
     int bottomY = 500;
-    int WIDTH = 0;
-    int HEIGHT = 0;
+    int WIDTH = 1;
+    int HEIGHT = 1;
     int X = 10;
     int Y = 10;
 
@@ -28,14 +28,15 @@ public class active extends PApplet {
         frameRate(30);
     }
     public void draw(){
-        fill(255, 0, 0, 63);
-        rect(leftX, topY, WIDTH, HEIGHT);
-        fill(0, 0, 255, 63);
-        rect(bottomX, bottomY, WIDTH, HEIGHT);
-        fill(0, 255, 0, 63);
-        rect(bottomX, topY, WIDTH, HEIGHT);
-        fill(255, 0, 255, 63);
-        rect(leftX, bottomY, WIDTH, HEIGHT);
+        stroke(1);
+        fill(255, 0, 0, 255 - WIDTH);
+        ellipse(leftX, topY, WIDTH, HEIGHT);
+        fill(0, 0, 255, 255-WIDTH);
+        ellipse(bottomX, bottomY, WIDTH, HEIGHT);
+        fill(0, 255, 0, 255-WIDTH);
+        ellipse(bottomX, topY, WIDTH, HEIGHT);
+        fill(255, 0, 255, 255-WIDTH);
+        ellipse(leftX, bottomY, WIDTH, HEIGHT);
 
         WIDTH += 10;
         HEIGHT += 10;
@@ -45,32 +46,37 @@ public class active extends PApplet {
         bottomY -= Y;
     }
     public void mousePressed(){ // Initiated when mouse is pressed or pressed and held
+        stroke(1);
         fill(255, 255, 0);
         rect(100, 100, 100, 100);
     }
-    public void mouseClicked(){  // Initiated when mouse is pressed AND released (rect() created upon release)
+    public void mouseClicked(){
+        stroke(1);                 // Initiated when mouse is pressed AND released (rect() created upon release)
         fill(255, 0, 255);        // If mouse is pressed and moved, then released, will not initiate
         rect(400, 400, 100, 100);
     }
     public void mouseDragged(){  // Initiated when mouse is held and moved
+        stroke(1);
         fill(0, mouseX, mouseY, 63);
         rect(mouseX, mouseY, 200, 200);
     }
     public void mouseMoved(){
-        fill(mouseX, mouseY, mouseX, 63);
+        noStroke();
+        fill(70, mouseY, mouseX, 20);
         ellipse(mouseX, mouseY, 150, 150);
     }
 
     public void keyPressed(){
-        background(0); // Clears the background
+        background(255); // Clears the background
     }
 
     public void keyTyped() {  // Behaves same as keyPressed()
+        stroke(1);
         fill(0, 0, 255);
         rect(0, 0, 200, 200);
     }
     public void keyReleased(){
-        background(255);
+        background(0);
     }
 
 
